3eee3653d18514dae112a49907d99367a4173715
xx19898 <zomg13912111@gmail.com>
2023-08-16 22:49:26 +0300

    done 13.14

--- part13/note-backend/index.js
+++ part13/note-backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const port = 3000
 const app = express()
 
-
+app.use(express.json())
 
 
 
@@ -13,15 +13,25 @@ app.get('/blogs',async (req,res) => {
     res.send(blogs)
 })
 
-app.post('/blogs', async ({body},res) => {
+app.post('/blogs', async (req,res) => {
+    console.log({req})
+    const body = req.body
+    console.log({body})
     const newBlog = await Blog.create({
         author:body.author,
         url:body.url,
         title:body.title,
         likes:body.likes
     })
-
     res.send(newBlog)
+})
+
+app.delete('/blogs/:id', async (req,res) => {
+    const id = req.params.id
+    const deletedBlog = await Blog.destroy({where:{
+        id: id
+    }})
+    res.sendStatus(200).send(deletedBlog)
 })
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
